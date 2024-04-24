9e279b0d25b0ef686ffe8d954c28a0004235861b
khanhphan1102 <dongian98@gmail.com>
2024-04-24 09:38:48 +0700

    up

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,18 @@
 
+// DOM
+function layThongTinTuForm() {
+    let maSv = document.querySelector("#txtMaSV").value;
+    let tenSv = document.querySelector("#txtTenSV").value;
+    let email = document.querySelector("#txtEmail").value;
+    let matKhau = document.querySelector("#txtPass").value;
+    let toan = document.querySelector("#txtDiemToan").value * 1;
+    let ly = document.querySelector("#txtDiemLy").value * 1;
+    let hoa = document.querySelector("#txtDiemHoa").value * 1;
+
+    let sv = new Sinhvien(maSv, tenSv, email, matKhau, toan, ly, hoa);
+    
+    return sv;
+}
 
 // RENDER DSSV
 function renderDssv(svArr) {
@@ -25,17 +39,4 @@ function renderDssv(svArr) {
     document.getElementById("tbodySinhVien").innerHTML = contentHTML;
 }
 
-// DOM
-function layThongTinTuForm() {
-    let maSv = document.querySelector("#txtMaSV").value;
-    let tenSv = document.querySelector("#txtTenSV").value;
-    let email = document.querySelector("#txtEmail").value;
-    let matKhau = document.querySelector("#txtPass").value;
-    let toan = document.querySelector("#txtDiemToan").value * 1;
-    let ly = document.querySelector("#txtDiemLy").value * 1;
-    let hoa = document.querySelector("#txtDiemHoa").value * 1;
 
-    let sv = new Sinhvien(maSv, tenSv, email, matKhau, toan, ly, hoa);
-    
-    return sv;
-}
